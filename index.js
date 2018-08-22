b67adda9c2417ae4c6c33358dba59d6307119084
syntaxhacker <30970495+syntaxhacker@users.noreply.github.com>
2018-08-22 23:49:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const Discord = require('discord.js');
+const keep_alive = require('./keep_alive.js')
+const client = new Discord.Client();
+const token = process.env.DISCORD_BOT_SECRET;
+
+client.on('ready', () => {
+  console.log("I'm in");
+  console.log(client.user.username);
+});
+
+client.on('message', msg => {
+    if (msg.author.id != client.user.id) {
+        msg.channel.send(msg.content.split('').join(''));
+    }
+});
+
+client.login(token);
